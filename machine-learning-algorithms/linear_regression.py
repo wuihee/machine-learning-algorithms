94078d9d169fa32d9affb69a4029ba4c79cc8428
wuihee <wuihee@gmail.com>
2024-06-08 01:53:11 -0700

    Updated docs

--- machine-learning-algorithms/linear_regression.py
+++ machine-learning-algorithms/linear_regression.py
@@ -3,60 +3,56 @@ import numpy as np
 
 class LinearRegression:
     """
-    Linear Regression model.
+    Linear regression model.
 
     Attributes:
-        lr (float): Learning rate for gradient descent.
-        n_iters (int): Number of iterations for gradient descent.
-        weights (numpy.ndarray): Weights of the model.
-        bias (float): Bias term of the model.
+        lr (float): Model's learning rate (alpha).
+        n_iters (int): The number of iteration the model updates for gradient descent.
+        weights (np.ndarray): The coefficients for each feature.
+        bias (float): The constant added at the end of the model's prediction.
     """
 
     def __init__(self, lr=0.001, n_iters=1000):
         """
-        Initializes the LinearRegression model with given learning rate and number of iterations.
-
-        Args:
-            lr (float, optional): Learning rate. Defaults to 0.001.
-            n_iters (int, optional): Number of iterations. Defaults to 1000.
+        Initializes linear regression model with a learning rate and number of iterations.
         """
         self.lr = lr
         self.n_iters = n_iters
         self.weights = None
         self.bias = None
 
-    def fit(self, X: np.ndarray, y: np.ndarray):
+    def fit(self, X, y):
         """
-        Trains the LinearRegression model using the given training data.
+        Trains the model given input features and target values.
 
         Args:
-            X (numpy.ndarray): Training data of shape (n_samples, n_features).
-            y (numpy.ndarray): Target values of shape (n_samples,).
+            X (np.ndarray): Training data of shape (n_samples, n_features).
+            y (np.ndarray): Target values of shape (n_samples).
         """
-        n_samples, n_features = X.shape
-        self.weights = np.zeros(n_features)
+        num_samples, num_features = X.shape
+        self.weights = np.zeros(num_features)
         self.bias = 0
 
-        # Apply gradient descent
+        # Apply gradient descent.
         for _ in range(self.n_iters):
-            y_predicted = np.dot(X, self.weights) + self.bias
+            y_predict = np.dot(X, self.weights) + self.bias
 
-            # Compute derivatives
-            dw = (1 / n_samples) * np.dot(X.T, (y_predicted - y))
-            db = (1 / n_samples) * np.sum(y_predicted - y)
+            # Calculate derivatives.
+            dw = (1 / num_samples) * np.dot(X.T, (y_predict - y))
+            db = (1 / num_samples) * np.sum(y_predict - y)
 
-            # Update parameters
+            # Update model parameters.
             self.weights -= self.lr * dw
             self.bias -= self.lr * db
 
-    def predict(self, X: np.ndarray):
+    def predict(self, X):
         """
-        Predicts the target values using the trained LinearRegression model.
+        Predicts the target values.
 
         Args:
-            X (numpy.ndarray): Input data of shape (n_samples, n_features).
+            X (np.ndarray): Training data of shape (n_samples, n_features).
 
         Returns:
-            numpy.ndarray: Predicted target values of shape (n_samples,).
+            np.ndarray: Predicted target values of shape (n_samples).
         """
         return np.dot(X, self.weights) + self.bias
